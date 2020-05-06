dca2cd3b1b13eaff3f8bf44e3b09a12f57ee4893
Alexandre Dubray <alexandre.dubray@uclouvain.be>
2020-05-06 11:46:58 +0200

    add some more type info for ComposedShape

--- shared/src/main/scala/DSLDemo/Shapes.scala
+++ shared/src/main/scala/DSLDemo/Shapes.scala
@@ -2,7 +2,7 @@ package DSLDemo
 
 sealed trait Shape {
   type A <: Shape
-  def and(s: Shape) : ComposedShape = ???
+  def and(s: Shape) : ComposedShape[Shape] = ???
   def change(property: CanvasElementModifier[A]): Unit = ???
   def moveX(v: Int): Unit = ???
 
@@ -15,11 +15,12 @@ sealed trait ShapeAttributes {
   // Add more attributes here
 }
 
-case class ComposedShape(var l: List[Shape]) extends Shape {
-  def map(f: Shape => Shape) : ComposedShape = ???
-  def flatMap(f: Shape => Iterable[Shape]) : ComposedShape = ???
-  def foreach[B](f: Shape => B) : Unit = ???
-  def apply(i: Int): Shape = ???
+case class ComposedShape[T <: Shape](var l: List[T]) extends Shape {
+  type A = T
+  def map[B <: Shape](f: T => B) : ComposedShape[B] = ???
+  def flatMap[B <: Shape](f: T => Iterable[B]) : ComposedShape[B] = ???
+  def foreach[B](f: T => B) : Unit = ???
+  def apply(i: Int): T = ???
 }
 
 case class Rectangle(var x: Int, var y: Int, var width: Int, var height: Int) extends Shape with ShapeAttributes {
